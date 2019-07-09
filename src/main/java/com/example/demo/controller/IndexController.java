ee12d681cdaaf4f1d48c0db4baadc42d4b51c08e
deng <dzbv@sina.com>
2019-07-09 17:35:12 +0800

    17：35

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	@RequestMapping("/")
 	public String index() {
-		return "回滚!";
+		return "jenkins 持续集成 ！";
 	}
 	
 	
