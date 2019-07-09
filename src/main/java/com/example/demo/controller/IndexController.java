edb2de8736eb94be598fc657997c0e5fa1855121
deng <dzbv@sina.com>
2019-07-09 15:25:20 +0800

    ...

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	@RequestMapping("/")
 	public String index() {
-		return "hello jenkins";
+		return "hello jenkins...";
 	}
 	
 	
